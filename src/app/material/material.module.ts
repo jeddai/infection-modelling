1bcd373cc5c2e7b49aba16effd5c9ecec0ae1d9e
Austin Humphreys <ahumphreys3@gmail.com>
2020-03-17 18:03:56 -0500

    fix mobile view and add descriptions

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-    imports: [
-        MatToolbarModule,
-        MatGridListModule,
-        MatTooltipModule
-    ],
-    exports: [
-        MatToolbarModule,
-        MatGridListModule,
-        MatTooltipModule
-    ]
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatListModule
+  ]
 })
 export class MaterialModule {}
